ca32e702212890ef93f3609f9a9e062182e7e1d2
JakeCollen <collej2@rpi.edu>
2024-11-07 17:54:41 -0500

    lab8 bug
    fixed lab8 bugs that wasn't letting projects read the json

--- projects/labs/lab08/lab08resources/lab8.js
+++ projects/labs/lab08/lab08resources/lab8.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
               dataType: "json",
               success: function(responseData){
                var output = "<ul>";  
-               $.each(responseData.labListItem, function(labListItem) {
+               $.each(responseData.labListItem, function(i, labListItem) {
                 output += '<li><a href="' + labListItem.page + '">';
                 output += labListItem.number + "-";
                 output += labListItem.title;
