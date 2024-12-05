41e782a5e29fbdd58bbb9cc4582044afaf86c485
JakeCollen <collej2@rpi.edu>
2024-12-05 15:42:44 -0500

    database creation
    create/insert sql files to generate tables

---
+++ quiz3/resources/projectTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `myProjects` (
+   `projectNumber` int(10) unsigned NOT NULL AUTO_INCREMENT,
+   `projectTitle` varchar(40) NOT NULL,
+   `projectLink` varchar(40) NOT NULL,
+   PRIMARY KEY (`projectNumber`)
+);
---
+++ quiz3/resources/tableInserts.sql
@@ -0,0 +1,23 @@
+INSERT INTO `myLabs` (`labNumber`, `labTitle`, `labLink`) VALUES
+(1, "GitHub and Azure", "labs/lab01/lab01.html"),
+(2, "Building a Resume", "labs/lab02/lab02.html"),
+(3, "Personal Website", "labs/lab03/lab03.html"),
+(4, "XML and RSS", "labs/lab04/lab04.html"),
+(5, "JavaScript", "labs/lab05/lab05.html"),
+(6, "JQuery", "labs/lab06/lab06.html"),
+(7, "Group Project Mockup", "labs/lab07/lab07.html"),
+(8, "Dynamic Website", "labs/lab08/lab08.html"),
+(9, "PHP and MySql", "labs/lab09/lab09.html"),
+(10, "Moving to Production", "labs/lab10/lab10.html");
+
+INSERT INTO `myProjects` (`projectNumber`, `projectTitle`, `projectLink`) VALUES
+(1, "Intro to ITWS Labs", "labs/index.php"),
+(2, "Intro to ITWS Group Project", "../../grp/index.php");
+
+INSERT INTO `myFooter` (`footerId`, `footerText`) VALUES
+(1, "Jake Collen-ITWS @ RPI");
+
+INSERT INTO `mySiteUsers` (`userId`, `username`, `password`) VALUES
+(1, "collej2", "JaysonTatum0$"),
+(2, "rplotka", "JakeServer"),
+(3, "mojiso", "JakeServer");
