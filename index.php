47e83cfabc6fc6f09f368ca6e8a5a2ba85795e27
JakeCollen <collej2@rpi.edu>
2024-12-06 14:22:16 -0500

    welcome message 3
    trying to fix why index.php won't load with login check function

--- index.php
+++ index.php
@@ -3,6 +3,23 @@ session_start();
 
 	include("connection.php");
 	include("functions.php");
+	if(isset($_SESSION['userId']))
+	{
+
+		$id = $_SESSION['userId'];
+		$query = "select * from users where userId = '$id' limit 1";
+
+		$result = mysqli_query($conn,$query);
+		if($result && mysqli_num_rows($result) > 0)
+		{
+
+			$user_data = mysqli_fetch_assoc($result);
+			return $user_data;
+		}
+	}
+
+	header("Location: quiz3/resources/login.php");
+	die;
 ?>
 
 <?php
