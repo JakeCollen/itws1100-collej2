578448c286b0c55e73ef5c298e6bfe87ed5d86ba
JakeCollen <collej2@rpi.edu>
2024-12-06 14:13:48 -0500

    welcome message 2
    trying to find a way to make welcome message appear when logged in

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+	$user_data = check_login($conn);
+
+?>
+
 <?php
 include('quiz3/resources/doc.php');
 ?>
@@ -12,7 +22,6 @@ Jake's Website
 <li><a href="index.php">Home</a></li>
 <li><a href="projects/projects.html">Projects</a></li>
 <li><a href="quiz3/resources/login.php">Login</a></li>
-Hello, <?php echo $user_data['username']; ?>
 
 <?php
 include('quiz3/resources/resume.php');
