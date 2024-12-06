9925a63c1fb66caa4acd0ff9f15f539cb4be4dad
JakeCollen <collej2@rpi.edu>
2024-12-06 14:02:19 -0500

    testing welcome message
    added welcome message to index.php after a user logs in

--- index.php
+++ index.php
@@ -11,11 +11,12 @@ include('quiz3/resources/header.php');
 Jake's Website
 <li><a href="index.php">Home</a></li>
 <li><a href="projects/projects.html">Projects</a></li>
+<li><a href="quiz3/resources/login.php">Login</a></li>
 
 <?php
 include('quiz3/resources/resume.php');
 ?>
-
+Hello, <?php echo $user_data['username']; ?>
 <?php
 include('quiz/resources/closing.php');
 ?>
