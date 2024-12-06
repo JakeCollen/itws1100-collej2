497805f23c331b38580b44ba79d3834e4c336745
JakeCollen <collej2@rpi.edu>
2024-12-06 10:50:06 -0500

    projects php
    Created projects.php, should generate projects page correctly

--- quiz3/resources/projectTable.sql
+++ quiz3/resources/projectTable.sql
@@ -2,5 +2,6 @@ CREATE TABLE `myProjects` (
    `projectNumber` int(10) unsigned NOT NULL AUTO_INCREMENT,
    `projectTitle` varchar(40) NOT NULL,
    `projectLink` varchar(40) NOT NULL,
+   `projectClass` varchar(40) NOT NULL,
    PRIMARY KEY (`projectNumber`)
 );
