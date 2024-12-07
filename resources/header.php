f8d609f7657d2fdacaeada1da42225fc49b410b0
JakeCollen <collej2@rpi.edu>
2024-12-06 22:08:50 -0500

    link issue

--- resources/header.php
+++ resources/header.php
@@ -1,8 +1,8 @@
  
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>   
-    <link href="https://collej2rpi.eastus.cloudapp.azure.com/iit/resources/header.css" rel="stylesheet" type="text/css"/>
-    <link href="https://collej2rpi.eastus.cloudapp.azure.com/iit/resources/resume.css" rel="stylesheet" type="text/css"/>
+    <link href="http://collej2rpi.eastus.cloudapp.azure.com/iit/resources/header.css" rel="stylesheet" type="text/css"/>
+    <link href="http://collej2rpi.eastus.cloudapp.azure.com/iit/resources/resume.css" rel="stylesheet" type="text/css"/>
   </head>
   <body>
     <ul class="header">
