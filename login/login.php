543774a12d37266e506ae3a6a0faeae9b6aabb00
JakeCollen <collej2@rpi.edu>
2024-12-06 22:14:26 -0500

    refine 1

--- login/login.php
+++ login/login.php
@@ -61,8 +61,8 @@ include('quiz3/resources/doc.php');
 <title>Jake's Website</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>   
-    <link href="../../resources/header.css" rel="stylesheet" type="text/css"/>
-    <link href="../../resources/login.css" rel="stylesheet" type="text/css"/>
+    <link href="../resources/header.css" rel="stylesheet" type="text/css"/>
+    <link href="../resources/login.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 <ul class="header">
--- projects/projects.php
+++ projects/projects.php
@@ -7,7 +7,6 @@
         <title>My Projects Home Page</title>
         <link href="../resources/header.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script type="text/javascript" src="../resources/projects_list.js"></script>
     </head>
 
 
@@ -27,12 +26,13 @@
             <li><a href="../index.html">Home</a></li>
             <li><a href="projects.html">Projects</a></li>
         </ul>
+        
         <?php
+            echo '<ul class="labs">';
             if ($connOk) {
                 $query = 'select * from myProjects order by projectNumber';
                 $result = $conn->query($query);
                 $numRecords = $result->num_rows;
-                echo '<ul class="labs">';
                 for ($i = 0; $i < $numRecords; $i++) {
                     $record = $result->fetch_assoc();
                     echo '<li>';
@@ -40,12 +40,11 @@
                     echo htmlspecialchars($record['projectClass']) . '-' . htmlspecialchars($record['projectTitle']);
                     echo '</a>';
                     echo '</li>';
-                echo '</ul>';
                 }
                 $result->free();
                 $conn->close();
             }
-
+            echo '</ul>';
         ?>
     </body>
 </html>
